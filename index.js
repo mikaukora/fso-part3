38b8f79830d55a003abfaa044e98759a111572d4
Mika Kaukoranta <mika.kaukoranta@mavericks.fi>
2021-06-30 10:59:47 +0300

    Cors for exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const e = require("express");
 const express = require("express");
+const cors = require('cors')
 const morgan = require('morgan');
 
+
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 
